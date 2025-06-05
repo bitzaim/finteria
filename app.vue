788a77bfda8264a6bab4af885e9be868d6652d2c
bitzaim <95851188+bitzaim@users.noreply.github.com>
2025-06-05 13:23:52 +0200

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup>
+import savePromocode from "@/utils/savePromocode";
+import { saveClickId, saveUtmSource } from "@/utils/saveClickIdAndUtmSource";
+
+savePromocode();
+saveClickId();
+saveUtmSource();
+</script>
+
+<template>
+  <div class="layout">
+    <NuxtLayout />
+  </div>
+</template>
